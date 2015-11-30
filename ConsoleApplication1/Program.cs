d97bfa1726d3c97586fb1257aa64d17ccb47caec
beauxjames <beauxjames@gmail.com>
2015-11-29 23:09:58 -0600

    Initial commit of content

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using SharedLibrary;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyValuePair<int, string>[] fooList =
+            {
+                new KeyValuePair<int, string>(3, "monkey"),
+                new KeyValuePair<int, string>(6, "banana"),
+                new KeyValuePair<int, string>(10, "aphid")
+            };
+
+            var testClass = new TestClass(int.MaxValue, fooList);
+            testClass.AsyncPrintNumbers();
+            do
+            {
+                foreach (var num in testClass.ResultList)
+                {
+                    System.Console.WriteLine(num);
+                }
+            } while (!testClass.IsComplete);
+            
+            System.Console.ReadKey();
+        }
+    }
+}
+
+// Unit tests to prove the '15' case -- ensure it never breaks
+// Tests to ensure stability over time
+
+// pass in their own word/number pairs 
+
+// EC -- add a build script and set up Git Src Control in a way that a team could work in a continuous integration env. 
+// post on Git and send to Rayne
+
---
+++ EarthShatteringTests/EarthShatteringTestNumeroUno.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedLibrary;
+
+namespace EarthShatteringTests
+{
+    [TestClass]
+    public class EarthShatteringTestNumeroUno
+    {
+        [TestMethod]
+        public void test_the_persistence_of_fifteen ()
+        {
+            var testClass = new TestClass(14);
+
+            testClass.PrintNumbers();
+            Assert.IsTrue(testClass.ResultList.Count < 15, "there should be LT 15 elements if the upper bound is LT 15");
+
+            testClass.ResetUpperBound(15);
+            Assert.IsTrue(testClass.ResultList.Count == 15 && testClass.ResultList.Last() == "15", "there should be 15 elements and the last element should be 15 if the upper bound EQ 15");
+
+            testClass.ResetUpperBound(20);
+            Assert.IsTrue(testClass.ResultList.Count > 15, "there should be GT 15 elements if the upper bound is GT 15");
+
+            testClass.ResetNumStrPairs(new List<KeyValuePair<int, string>> { new KeyValuePair<int, string>(3, "foo") });
+            Assert.AreEqual("foo", testClass.ResultList[14], "value at 15 mod 3 Should equal foo");
+
+            testClass.ResetNumStrPairs(new List<KeyValuePair<int, string>> { new KeyValuePair<int, string>(5, "bar") });
+            Assert.AreEqual("bar", testClass.ResultList[14], "value at 15 mod 5 Should equal bar");
+
+            testClass.ResetNumStrPairs(new List<KeyValuePair<int, string>>
+            {
+                new KeyValuePair<int, string>(3, "foo"),
+                new KeyValuePair<int, string>(5, "bar")
+            });
+            Assert.AreEqual("foo bar", testClass.ResultList[14], "value at 15 mod 3 Should equal foo bar");
+        }
+
+        [TestMethod]
+        public void test_stability_over_time()
+        {
+            var testClass = new TestClass(4096);
+
+            testClass.PrintNumbers();
+        }
+    }
+}
